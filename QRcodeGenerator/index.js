aede722d34662839bd3b01310548fd323975965d
rithesh-n <ritheshan18@gmail.com>
2024-12-10 23:55:16 +0530

    qr-code generater was run using node

--- QRcodeGenerator/index.js
+++ QRcodeGenerator/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import inquirer from "inquirer"
 import qr from "qr-image"
 import fs from "fs"
@@ -33,11 +33,6 @@ fs.writeFile('url.txt', url, (err) => {
 /* 
 
 1. Use the inquirer npm package to get user input.
-
-=======
-/* 
-1. Use the inquirer npm package to get user input.
->>>>>>> 687e76da50d56d87aec9d4d44fc461aca8505fc8
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
