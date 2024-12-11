3902db390746e7513660f3aaaf8f9658f4d01435
rithesh-n <ritheshan18@gmail.com>
2024-12-11 19:00:47 +0530

    added package.json file

--- Middleware/index1.js
+++ Middleware/index1.js
@@ -6,10 +6,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
 app.post("/submit",(req,res)=>{
   console.log(req.body);
 });
